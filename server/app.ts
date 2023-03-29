fb43e4ed63187331f83279f16988db9bbe09cfb3
Andreas Cross <andreasocross@yahoo.com>
2023-03-29 11:32:48 +0200

    origin reset

--- server/app.ts
+++ server/app.ts
@@ -18,8 +18,8 @@ const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 app.use(cors({
-origin: 'http://127.0.0.1:5173'
-// origin: 'https://silver-macaron-ef4571.netlify.app'
+// origin: 'http://127.0.0.1:5173'
+origin: 'https://silver-macaron-ef4571.netlify.app'
 }));
 
 
