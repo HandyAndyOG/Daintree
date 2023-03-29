0e907d04dccea07f4b228e59d2d8290e7c69df70
Andreas Cross <andreasocross@yahoo.com>
2023-03-29 12:19:32 +0200

    env

--- server/app.ts
+++ server/app.ts
@@ -18,9 +18,10 @@ const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://127.0.0.1:5173' || 'https://silver-macaron-ef4571.netlify.app'
+  origin: `${process.env.FRONT_URL}`
 }));
 // origin: 'https://silver-macaron-ef4571.netlify.app'
+// origin: 'http://127.0.0.1:5173'
 
 
 const authenticateToken = (req: any, res: any, next: any) => {
